e4bd54761ab547192942b62f4e030929b90b7555
Benjamin Jackson <benj@betterrentals.com.au>
2024-11-09 22:14:52 +1100

    ..

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,32 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["open", "close", "indexContainer", "menuContainer"]
+  static targets = ["open", "close", "mainDiv", "menuContainer"]
 
   connect() {
     console.log("connected!");
   }
 
   menuOpen() {
-    console.log("opening menu!");
     this.openTarget.classList.add("hidden-flip-left");
     this.closeTarget.classList.add("block-flip-right");
     this.openTarget.classList.remove("block-flip-right");
     this.closeTarget.classList.remove("hidden-flip-left");
-    this.indexContainerTarget.classList.add("hidden");
-    this.indexContainerTarget.classList.remove("block");
-    this.menuContainerTarget.classList.remove("hidden");
+    this.mainDivTarget.classList.add("hidden");
+    this.mainDivTarget.classList.remove("block");
     this.menuContainerTarget.classList.add("block");
+    this.menuContainerTarget.classList.remove("hidden");
   }
 
   menuClose() {
-    console.log("closing menu!");
     this.closeTarget.classList.add("hidden-flip-left");
-    this.openTarget.classList.add("block-flip-right");
     this.closeTarget.classList.remove("block-flip-right");
     this.openTarget.classList.remove("hidden-flip-left");
-    this.indexContainerTarget.classList.remove("hidden");
-    this.indexContainerTarget.classList.add("block");
+    this.openTarget.classList.add("block-flip-right");
+    this.mainDivTarget.classList.remove("hidden");
+    this.mainDivTarget.classList.add("block");
     this.menuContainerTarget.classList.add("hidden");
     this.menuContainerTarget.classList.remove("block");
   }
